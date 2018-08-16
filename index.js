cc3420be0a2c23003a3a0db45a97fbbb5f45ca9b
Matthew Crooks <mfcrooks@gmail.com>
2018-08-16 01:24:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ const logDriversByHometown = function (drivers, location) {
     }
   });
 };
+
+const driversByRevenue = function (drivers) {
+  return drivers.slice().sort(function (driverOne, driverTwo) {
+    return driverOne.revenue - driverTwo.revenue;
+  });
+};
